b60144a41397398f30336b4f22cde95ff505187b
2325272909 <71299599+2325272909@users.noreply.github.com>
2020-09-22 16:30:23 +0800

    Add files via upload

---
+++ Picture.java
@@ -0,0 +1,88 @@
+package Shape;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+
+
+
+//图形
+public class Picture
+{
+    private Square wall;
+    private Square window;
+    private Triangle roof;
+    private Circle sun;
+
+
+    public Picture()
+    {
+        // nothing to do... instance variables are automatically set to null
+    }
+
+
+    public void draw()
+    {
+        wall = new Square();
+        wall.moveVertical(80);
+        wall.changeSize(100);
+        wall.makeVisible();
+
+        window = new Square();
+        window.changeColor("black");
+        window.moveHorizontal(20);
+        window.moveVertical(100);
+        window.makeVisible();
+
+        roof = new Triangle();
+        roof.changeSize(50, 140);
+        roof.moveHorizontal(60);
+        roof.moveVertical(70);
+        roof.makeVisible();
+
+        sun = new Circle();
+        sun.changeColor("yellow");
+        sun.moveHorizontal(180);
+        sun.moveVertical(-10);
+        sun.slowMoveHorizontal(1);
+        sun.changeSize(60);
+        sun.makeVisible();
+    }
+
+
+    public void setBlackAndWhite()
+    {
+        if(wall != null)   // only if it's painted already...
+        {
+            wall.changeColor("black");
+            window.changeColor("white");
+            roof.changeColor("black");
+            sun.changeColor("black");
+        }
+    }
+
+
+    public void setColor()
+    {
+        if(wall != null)   // only if it's painted already...
+        {
+            wall.changeColor("red");
+            window.changeColor("black");
+            roof.changeColor("green");
+            sun.changeColor("yellow");
+        }
+    }
+
+    public static void main(String[] args) {
+        Picture picture = new Picture();
+        //        picture.setBlackAndWhite();
+        picture.draw();
+    }
+
+}
+
